b0707699bf63bddb1130e8614c21a8bdcfa40505
linjieshang <linjieshang@berkeley.edu>
2025-08-09 00:37:15 -0700

    Finish admin dashboard

--- app/appwrite/trips.ts
+++ app/appwrite/trips.ts
@@ -1,2 +1,37 @@
-// Logic for fetching all the trips or a trip by id
-export 
+import { Query } from "appwrite"
+import { appwriteConfig, database } from "./client"
+
+// Logic for fetching all the trips 
+export const getAllTrips = async (limit: number, offset: number) => {
+    const allTrips = await database.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.tripCollectionId,
+        [Query.limit(limit), Query.offset(offset), Query.orderDesc('createdAt')]
+    )
+
+    if (allTrips.total === 0) {
+        console.error('No trips found');
+        return { allTrips: [], total: 0 }
+    }
+
+    return {
+        allTrips: allTrips.documents,
+        total: allTrips.total,
+    }
+}
+
+// Fetch a trip by id
+export const getTripById = async (tripId: string) => {
+    const trip = await database.getDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.tripCollectionId,
+        tripId
+    );
+
+    if (!trip.$id) {
+        console.log('Trip not found')
+        return null;
+    }
+
+    return trip;
+}
---
+++ app/routes/root/travel-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const travelPage= () => {
+  return (
+    <div>travelPage</div>
+  )
+}
+
+export default travelPage
